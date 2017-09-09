9f027db8e4c1de0786ef04b633258fdd181e27af
TheOnlyArtz <gamesil456@gmail.com>
2017-09-09 19:50:10 +0300

    XO

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
-const express    = require('express'),
-      app        = express(),
-      path       = require('path'),
-      bodyParser = require('body-parser'),
+const path = require('path');
+
+const express = require('express');
+
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(process.env.PORT || 3000, async () => {
+	console.log('Started a server on port 3000');
+});

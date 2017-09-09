cf0ec559872167d655dedc1522f09750fd373596
TheOnlyArtz <gamesil456@gmail.com>
2017-09-09 20:07:28 +0300

    Routing home.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 
 const app = express();
 const bodyParser = require('body-parser');
+const routes = require('./routes');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -11,6 +12,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Routes...
+app.get('/', routes.home)
 
 app.listen(process.env.PORT || 3000, async () => {
 	console.log('Started a server on port 3000');

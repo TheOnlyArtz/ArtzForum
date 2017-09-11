231f7c8187af2e5c4c297e3d935a41ff1e5dbe8d
TheOnlyArtz <gamesil456@gmail.com>
2017-09-11 18:07:50 +0300

    s

--- server.js
+++ server.js
@@ -11,10 +11,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/css')));
 
 //Routes...
 app.get('/', routes.home)
+app.get('/subject/subject', routes.subject)
 
 app.listen(process.env.PORT || 3000, async () => {
 	console.log('Started a server on port 3000');

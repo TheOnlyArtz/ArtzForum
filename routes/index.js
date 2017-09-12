a0605f4bceb8a75f3e50893194701ad8403bd056
TheOnlyArtz <gamesil456@gmail.com>
2017-09-12 15:36:49 +0300

    API

--- routes/index.js
+++ routes/index.js
@@ -9,8 +9,28 @@ exports.subject = function (req, res) {
 }
 
 exports.subjectPost = function (req, res) {
-  console.log(req.params.subject);
   res.render(`subjectPost`, {
     subject : req.params.subject
   })
+}
+
+exports.subjectSubmittedPost = function (req, res) {
+  let title = req.body.postTitle
+  let content = req.body.postContent
+  res.render(`subjectPost`, {
+    subject : req.params.subject
+  })
+
+  function makeid() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < 9; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+  }
+
+  const URI_ID = makeid();
+
 }
--- server.js
+++ server.js
@@ -5,6 +5,23 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const routes = require('./routes');
+global.r = require('rethinkdbdash')({
+	db: 'Forum',
+	discovery: false,
+	pool: true,
+	buffer: 100,
+	max: 4000,
+	timeout: 60,
+	pingInterval: -1,
+	timeoutError: 1000,
+	timeoutGb: 60 * 60 * 1000,
+	maxExponent: 6,
+	silent: false,
+	servers: [
+    {host: 'localhost', port: 28015}
+	],
+	optionalRun: true
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -17,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public/css')));
 app.get('/', routes.home)
 app.get('/subject/subject', routes.subject)
 app.get('/subject/:subject/newPost', routes.subjectPost)
+app.post('/subject/:subject/newPost', routes.subjectSubmittedPost)
 app.listen(process.env.PORT || 3000, async () => {
 	console.log('Started a server on port 3000');
 });

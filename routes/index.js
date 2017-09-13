66121fb9901a204716c8595f87e45a26e684aefe
TheOnlyArtz <gamesil456@gmail.com>
2017-09-13 18:32:11 +0300

    some fixes

--- routes/index.js
+++ routes/index.js
@@ -1,94 +1,92 @@
 exports.home = async function (req, res) {
-  res.render('home')
-}
+	res.render('home');
+};
 
 exports.subject = async function (req, res) {
-  const exists = await r.table('Subjects').get(req.params.subject)
-  if (exists === null) {
-    return res.redirect('/*')
-  }
-  const recentPosts = await r.table('Posts').getAll(req.params.subject, {index : "subject"}).run()
-  res.render('subject', {
-    subject : req.params.subject,
-    recentPosts : recentPosts,
-  })
-}
+	const exists = await r.table('Subjects').get(req.params.subject);
+	if (exists === null) {
+		return res.redirect('/*');
+	}
+	const recentPosts = await r.table('Posts').getAll(req.params.subject, {index: 'subject'}).run();
+	res.render('subject', {
+		subject: req.params.subject,
+		recentPosts
+	});
+};
 
 exports.subjectPost = async function (req, res) {
-  res.render(`subjectPost`, {
-    subject : req.params.subject
-  })
-}
+	res.render(`subjectPost`, {
+		subject: req.params.subject
+	});
+};
 
 exports.subjectSubmittedPost = async function (req, res) {
-  let title = req.body.postTitle
-  let content = req.body.postContent
-  let subject = req.body.postSubject
-  // res.render(`subjectPost`, {
+	const title = req.body.postTitle;
+	const content = req.body.postContent;
+	const subject = req.body.postSubject;
+  // Res.render(`subjectPost`, {
   //   subject : req.params.subject
   // })
 
+	const URI_ID = await makeid();
+	const URL = `/subject/${subject}/post/${URI_ID}`;
 
-  const URI_ID = await makeid();
-  const URL = `/subject/${subject}/post/${URI_ID}`
-
-  try {
-    await r.table('Posts').insert({
-      postID : URI_ID,
-      postURI : URL,
-      subject : subject,
-      title : title,
-      content : content,
-      comments : []
-    });
-    res.redirect(URL)
-  } catch (e) {
-     res.redirect('SubjectFailure')
-  }
-}
+	try {
+		await r.table('Posts').insert({
+			postID: URI_ID,
+			postURI: URL,
+			subject,
+			title,
+			content,
+			comments: []
+		});
+		res.redirect(URL);
+	} catch (e) {
+		res.redirect('SubjectFailure');
+	}
+};
 
 exports.notFound = function (req, res) {
-  res.render('notFound')
-}
+	res.render('notFound');
+};
 
 exports.postPage = async function (req, res) {
-
-  const forumExists = await r.table('Subjects').get(req.params.subject)
-  const postExists = await r.table('Posts').getAll(req.params.subject, {index : "subject"})
-  const _POST = await r.table('Posts').getAll(req.params.subject, {index : "subject"})
-  .filter({postID : req.params.post}).run();
-  if (!_POST[0].postID || !forumExists === null || !postExists[0]) {
-    res.redirect('/*');
-    return;
-  }
-  res.render('postPage', {
-    title : _POST[0].title,
-    subject : _POST[0].subject,
-    comments : _POST[0].comments,
-    content : _POST[0].content,
-    postID : _POST[0].postID,
-  })
-}
+	const forumExists = await r.table('Subjects').get(req.params.subject);
+	const postExists = await r.table('Posts').getAll(req.params.subject, {index: 'subject'});
+	const _POST = await r.table('Posts').getAll(req.params.subject, {index: 'subject'})
+  .filter({postID: req.params.post}).run();
+	if (!_POST[0].postID || !forumExists === null || !postExists[0]) {
+		res.redirect('/*');
+		return;
+	}
+	res.render('postPage', {
+		title: _POST[0].title,
+		subject: _POST[0].subject,
+		comments: _POST[0].comments,
+		content: _POST[0].content,
+		postID: _POST[0].postID
+	});
+};
 
 exports.commentSubmitted = async function (req, res) {
-  const _POST = await r.table('Posts').getAll(req.params.subject, {index : "subject"})
-  .filter({postID : req.params.post}).run();
-  let appendToArray = async (table, uArray, doc) => r.table(table)
-    .filter({postID : req.body.postID})
-    .update(object => ({ [uArray]: object(uArray)
-    .default([]).append(doc) }))
+	const _POST = await r.table('Posts').getAll(req.params.subject, {index: 'subject'})
+  .filter({postID: req.params.post}).run();
+	const appendToArray = async (table, uArray, doc) => r.table(table)
+    .filter({postID: req.body.postID})
+    .update(object => ({[uArray]: object(uArray)
+    .default([]).append(doc)}))
     .run();
-  await appendToArray('Posts', 'comments', {content: req.body.commentContent})
-  await res.redirect(`/subject/${req.body.postSubject}/post/${req.body.postID}/`)
-
-}
+	await appendToArray('Posts', 'comments', {content: req.body.commentContent});
+	await res.redirect(`/subject/${req.body.postSubject}/post/${req.body.postID}/`);
+};
 
 async function makeid() {
-  var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	let text = '';
+	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-  for (var i = 0; i < 9; i++)
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
+	for (let i = 0; i < 9; i++) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
 
-  return text;
+	return text;
 }
--- server.js
+++ server.js
@@ -29,16 +29,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public/css')));
-//Get Routes...
-app.get('/', routes.home)
-app.get('/subject/:subject', routes.subject)
-app.get('/subject/:subject/newPost', routes.subjectPost)
-app.get('/subject/:subject/post/:post', routes.postPage)
-app.get('/*', routes.notFound)
+// Get Routes...
+app.get('/', routes.home);
+app.get('/subject/:subject', routes.subject);
+app.get('/subject/:subject/newPost', routes.subjectPost);
+app.get('/subject/:subject/post/:post', routes.postPage);
+app.get('/*', routes.notFound);
 
-//Post Routes...
-app.post('/subject/:subject/newPost', routes.subjectSubmittedPost)
-app.post('/subject/:subject/post/:post/submitted', routes.commentSubmitted)
+// Post Routes...
+app.post('/subject/:subject/newPost', routes.subjectSubmittedPost);
+app.post('/subject/:subject/post/:post/submitted', routes.commentSubmitted);
 app.listen(process.env.PORT || 3000, async () => {
 	console.log('Started a server on port 3000');
 });
